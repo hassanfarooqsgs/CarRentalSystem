f6275a4e6c77335d897ae53f8c40654adb2dc20a
hassanfarooq <hassanfarooqsgs@Gmail.com>
2023-03-03 14:32:35 +0500

    renew

--- src/main/java/com/example/CarRentalSystem/Booking.java
+++ src/main/java/com/example/CarRentalSystem/Booking.java
@@ -5,7 +5,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import lombok.Data;
+@Data
 @Entity
 @Table(name="Booking")
 public class Booking {
@@ -18,37 +19,8 @@ public class Booking {
     @Column private int  driver_hour;
 
 
-    public Integer getBooking_id() {
-        return booking_id;
-    }
 
-    public void setBooking_id(int booking_id) {
-        this.booking_id = booking_id;
-    }
-
-    public String getBooking_date() {
-        return booking_date;
-    }
-
-    public void setBooking_date(String booking_date) {
-        this.booking_date = booking_date;
-    }
-
-    public int getDriver_time() {
-        return driver_time;
-    }
-
-    public void setDriver_time(int driver_time) {
-        this.driver_time = driver_time;
-    }
-
-    public int getDriver_hour() {
-        return driver_hour;
-    }
-
-    public void setDriver_hour(int driver_hour) {
-        this.driver_hour = driver_hour;
     }
 
 
-}
+
--- src/main/java/com/example/CarRentalSystem/userr.java
+++ src/main/java/com/example/CarRentalSystem/userr.java
@@ -1,12 +1,20 @@
 package com.example.CarRentalSystem;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name="user")
 
 
 public class userr {
-
+   @Id @Column int user_id;
+    @Column String user_name;
+    @Column String user_email;
+    @Column int user_phn;
+    @Column String user_address;
 }
